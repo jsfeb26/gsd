1eab7ce1ce13f12e76e5bedd2eb24e452b0abc52
jsfeb26 <jsfeb26@gmail.com>
2015-09-17 07:16:39 -0400

    Finish ApiClient to make an api request for any request type based on passed in request

--- src/helper/ApiClient.js
+++ src/helper/ApiClient.js
@@ -1,3 +1,4 @@
+import superagent from 'superagent';
 import config from '../config';
 
 /*
@@ -8,13 +9,56 @@ import config from '../config';
  */
 class ApiClient_ {
   constructor(req) {
+    // loop through each request method
+    // return a new promise for each one
+    // if the request is successfull then resolve each promise
     ['get', 'post', 'put', 'patch', 'del']
       .forEach((method) => {
         this[method] = (path, options) => {
           return new Promise((resolve, reject) => {
+            const request = superagent[method](this.formatUrl(path));
 
+            if (options && options.params) {
+              request.query(options.params);
+            }
+
+            // if (isServer) {
+            //   // if there is a cookie set as the new request cookie
+            //   if (req.get('cookie')) {
+            //     request.set('cookie', req.get('cookie'));
+            //   }
+            // }
+
+            // if there is an options.data send it with the new request
+            if (options && options.data) {
+              request.send(options.data);
+            }
+
+            request.end((err, res) => {
+              if (err) {
+                reject(res.body || err);
+              }
+              else {
+                resolve(res.body);
+              }
+            });
           });
         };
       });
   }
+
+  formatUrl(path) {
+    const adjustedPath = path[0] !== '/' ? '/' + path : path;
+    if (isServer) {
+      // Preped host and port of the API server to the path
+      return 'http://localhost:' + config.apiPort + adjustedPath;
+    }
+
+    // Prepend '/api' to the relative URL, to proxy to API server
+    return '/api' + adjustedPath;
+  }
 }
+
+const ApiClient = ApiClient_;
+
+export default ApiClient;
